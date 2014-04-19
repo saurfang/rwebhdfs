6f5e97e3300564535e9798e2ccad003bf60b47a4
Saurfang <forest.fang@outlook.com>
2014-04-19 16:36:10 -0400

    Add webhdfs and mkdir
    Creates webhdfs S3 implementation. Adds S3 generic method: mkdir to
    create new directory.

---
+++ R/mkdir.R
@@ -0,0 +1,34 @@
+#' Create directory on a FileSystem
+#' 
+#' @return \code{TRUE} if seccessful, \code{FALSE} otherwise
+#' @title mkdir: Create Directory Function
+#' @param fs FileSystem object
+#' @param path a character vector that contains the path of folder to create
+#' @param ... other arguments
+#' @rdname mkdir
+#' @export mkdir
+mkdir <- function(fs, path, ...){
+  UseMethod("mkdir")
+}
+
+#' @rdname mkdir
+#' @method mkdir default
+#' @S3method mkdir default
+mkdir.default <- function(fs, path, ...){
+  warning("Unrecognized filesystem, invoking dir.create...")
+  dir.create(path, ...)
+}
+ 
+#' @rdname mkdir
+#' @method mkdir webhdfs
+#' @S3method mkdir webhdfs
+#' @importFrom jsonlite fromJSON
+mkdir.webhdfs <- function(fs, path, ...){
+  #Check path is absolute
+  if(!nzchar(path) || substring(path, 1, 1)!="/")
+    stop("Path must be non-empty and start with slash '/'")
+  
+  #XXX: Safe to assume webhdfs is legal here?
+  response <- fs$put(paste(path,"?op=MKDIRS"))
+  return(fromJSON(response)$boolean)
+}
---
+++ R/webhdfs.R
@@ -0,0 +1,86 @@
+#' Initialize WebHDFS Object
+#' 
+#' @section Enable WebHDFS
+#' To enable WebHDFS in your Hadoop Installation add the following configuration 
+#' to your hdfs_site.xml (requires Hadoop >0.20.205.0):
+#'
+#'  <property>
+#'  <name>dfs.webhdfs.enabled</name>
+#'  <value>true</value>
+#'  </property>  
+#'  
+#'  see: \url{https://issues.apache.org/jira/secure/attachment/12500090/WebHdfsAPI20111020.pdf}
+#' 
+#' @param namenode_host namenode host address
+#' @param namenode_port namenode host port
+#' @param hdfs_username webhdfs access username
+#' @param securityON \code{TRUE} if HDFS security is on, \code{FALSE} otherwise
+#' @param token Hadoop delegation token to use when security is ON. \code{NULL} implies Kerberos SPNEGO authentication if \code{securityON} is \code{TRUE}
+#' @return a webhdfs object that captures the configuration
+#' @importFrom RCurl httpPUT httpGET curlOptions
+#' @export
+webhdfs <- function(namenode_host, namenode_port, hdfs_username,
+                    securityON = FALSE, token = NULL){
+  #validate hostname
+  ValidIpAddressRegex <- "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+  ValidHostnameRegex <- "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])$"
+  if(!grepl(ValidIpAddressRegex, namenode_host) && !grepl(ValidHostnameRegex, namenode_host))
+    stop("hostname is not valid: ", namenode_host)
+  
+  #validate port number
+  tryCatch({
+    namenode_port <- as.integer(namenode_port)
+    if(is.na(namenode_port) || namenode_port < 1 || namenode_port > 65535)
+      stop("port number must between 1 and 65535: ", namenode_port)
+  }, warning = function(x){
+    stop("port must be a valid integer!")
+  })
+  
+  #validate non-empty token if supplied
+  if(!is.null(token) && !isTRUE(nzchar(token)))
+    warning("Delegation token is empty")
+  
+  #piece request URL to together
+  updateURL <- function(url, host, port, token, doas){
+    url <- paste0("http://",host,":",port,"/webhdfs/v1/",url)
+    if(isTRUE(nzchar(token)))
+      url <- paste0(url,"&token=",token)
+    else if(isTRUE(nzchar(doas)))
+      url <- paste0("url","&doas=",doas)
+    url
+  }
+  
+  structure(list(host=namenode_host, 
+                 port=namenode_port, 
+                 user=hdfs_username,
+                 security=securityON,
+                 token=token,
+                 put=function(url, content=NULL, doas=NULL, ...){
+                   opts <- list()
+                   if(securityON){
+                     if(is.null(token))
+                       opts <- curlOptions(username=":")
+                   }else{
+                     token <- NULL
+                   }
+                   url <- updateURL(url, namenode_host, namenode_port, token, doas)
+                  
+                   if(is.null(content))
+                     httpPUT(url, .opts=opts, ...)
+                   else
+                     httpPUT(url, content, .opts=opts, ...)
+                 },
+                 get=function(url, doas = NULL, ...){
+                   opts <- list()
+                   if(securityON){
+                     if(is.null(token))
+                       opts <- curlOptions(username=":")
+                   }else{
+                     token <- NULL
+                   }
+                   url <- updateURL(url, namenode_host, namenode_port, token, doas)
+                   
+                   httpGET(url, .opts=opts, ...)
+                 }),
+            class = "webhdfs")
+}
