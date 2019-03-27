56d63d205f5a3ab2035f7f481788d762aed130b5
Antonio Bibiano <antbbn@gmail.com>
2019-03-27 13:43:45 +0100

    Add trash option to delete_file
    The trash options allows to safely delete a file by moving it to
    the Trash instead of permanently delete it.
    By default it will be false to avoid breaking compatibility with
    previous versions.

--- R/delete_file.R
+++ R/delete_file.R
@@ -26,8 +26,22 @@ delete_file.default <- function(fs, path, recursive=FALSE, ...){
 #' @importFrom jsonlite fromJSON
 #' @include curl_webhdfs.R
 #' @export
-delete_file.webhdfs <- function(fs, path, recursive=FALSE, ...){  
-  url <- paste0(path,"?op=DELETE&recursive=",if(isTRUE(recursive)) "true" else "false")
-  response <- curl_webhdfs(fs, url, "DELETE" , ...)  
-  return(fromJSON(response)$boolean)
+delete_file.webhdfs <- function(fs, path, recursive=FALSE, trash=FALSE,...){
+  if (trash) {
+    response <- curl_webhdfs(fs, paste0(path,"?op=GETTRASHROOT"), "GET", ...)
+    trash_path <- fromJSON(response)$Path
+    if(!grep("^/", path)) {
+      path <- paste0(get_webhdfs_home(fs, ...), "/", path)
+    }
+    # Need to recereate the directory hierarchy under the Current trash
+    result <- mkdir(fs,paste0(trash_path,'/Current',dirname(path)))
+    if (result) {
+      result <- rename_file(fs,path,paste0(trash_path,'/Current',path))
+    }
+    return(result)
+  } else {
+    url <- paste0(path,"?op=DELETE&recursive=",if(isTRUE(recursive)) "true" else "false")
+    response <- curl_webhdfs(fs, url, "DELETE" , ...)
+    return(fromJSON(response)$boolean)
+  }
 }
